8f18128d38367266a9a063e9df80b8a3afd8b2df
maitha1-nule <nuleahmed6@gmail.com>
2024-06-11 23:14:57 +0300

    this was the session e had on Teusday about an into to dart programming

---
+++ index.dart
@@ -0,0 +1,118 @@
+void main(){
+  /*
+  multiline comment
+  */
+  //data types
+  //int ---> number --> whole numbers 
+  //double --> number --> decimal numbers
+  //String --> text ---> Nadia
+  //booline --> true or false
+  //var isTrue = true;
+  //variables
+  
+  //declaration of a variable
+  //data_type variable_name
+  //int age = 27;
+  //print(age);
+  //sting declaration
+  //const name = "Cynthia";
+  //final country = "Kenya"; 
+  //var name = "Westly";
+  //print(name);
+  //print(age);
+  //constants
+  //print("Helo, my name is $name and i am $age years old and i'm from $country");
+//print("Hello world");
+//var age = 27;
+//var age2 = 32;
+//print(age2 > age);
+//print(90 > 80);
+
+//conditional statements
+// if statement
+//if... else statement
+/*
+var people = 7;
+if (people < 2) {
+  print("The session cannot continue");
+  //The session will continue --> wesley, Brender and Enock
+  //nothing ---> Cynthia
+}
+else {
+  print("The session will continue");
+}
+*/
+//if... else if... else statement
+/*
+var marks = 70;
+if (marks >= 80 && marks <= 100) {
+  print("A");
+}
+else if(marks >= 70 && marks < 80) {
+  print("You got Grade B of $marks marks");
+}
+else if (marks >= 60 && marks < 70) {
+  print("C");
+}
+else if (marks >= 50 && marks < 60) {
+  print("D");
+}
+else if (marks >= 40 && marks < 50) {
+  print("E");
+}
+else if (marks >= 0 && marks < 40) {
+  print("F");
+}
+else {
+  print("Invalid marks");
+}
+*/
+
+//switch statement
+//switch(variable) { case value: break; default: }
+
+/* var marks = 70;
+switch(marks) {
+  case 80:
+  print("A");
+  break;
+  case 70:
+  print("B");
+  break;
+  case 60:
+  print("C");
+  break;
+  case 50:
+  print("D");
+  break;
+  case 40:
+  print("E");
+  break;
+  case 30:
+  print("F");
+  break;
+  default:
+  print("Invalid marks");
+}
+*/
+var vowel = "A";
+switch(vowel) {
+  case "A":
+  print("Vowel A");
+  break;
+  case "E":
+  print("Vowel E");
+  break;
+  case "I":
+  print("Vowel I");
+  break;
+  case "O":
+  print("Vowel O");
+  break;
+  case "U":
+  print("Vowel U");
+  break;
+  default:
+  print("Invalid vowel");
+}
+}
